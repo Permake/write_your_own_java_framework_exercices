578f4c37e2b08ed18c169acfc7811118cdba7fe7
Baptiste Mlynarz <baptisteml94@gmail.com>
2023-09-19 11:47:30 +0200

    TP2 fini

--- injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
+++ injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
@@ -1,6 +1,7 @@
 package com.github.forax.framework.injector;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
 import java.util.*;
 import java.util.function.Supplier;
 
@@ -48,17 +49,34 @@ public final class InjectorRegistry {
       .toList();
   }
 
+  private Optional<Constructor<?>> findInjectableConstructor (Class<?> type) {
+        var constructors = Arrays.stream(type.getConstructors())
+          .filter(constructor -> constructor.isAnnotationPresent(Inject.class))
+          .toList();
+        return switch (constructors.size()) {
+            case 0 -> Optional.empty();
+            case 1 -> Optional.of(constructors.get(0));
+            default -> throw new IllegalStateException("Too many injectable constructors " + type.getName());
+        };
+  }
   public <T> void registerProviderClass(Class<T> type, Class<? extends T> providerClass) {
     Objects.requireNonNull(type);
     Objects.requireNonNull(providerClass);
-    var constructor = Utils.defaultConstructor(providerClass);
+    var constructor = findInjectableConstructor(providerClass)
+            .orElseGet(() -> Utils.defaultConstructor(providerClass));
     var properties = findInjectableProperties(providerClass);
-      registerProvider(type, () -> {
-      var instance = Utils.newInstance(constructor);
+    registerProvider(type, () -> {
+      var arguments = Arrays.stream(constructor.getParameterTypes())
+              .map(this::lookupInstance).toArray();
+      var instance = Utils.newInstance(constructor, arguments);
       for (var property : properties) {
         Utils.invokeMethod(instance, property.getWriteMethod(), lookupInstance(property.getPropertyType()));
       }
-      return instance;
+      return providerClass.cast(instance);
     });
+  }
+
+  public <T> void registerProviderClass(Class<T> providerClass) {
+        registerProviderClass(providerClass, providerClass);
   }
 }
